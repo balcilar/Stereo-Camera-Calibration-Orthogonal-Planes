611400f10bcfb4be62b32453abd32a21312cff73
Muhammet Balcilar <muhammetbalcilar@gmail.com>
2018-07-12 12:01:27 +0200

    fixed left/rigth camera positions confusion

--- main.m
+++ main.m
@@ -67,8 +67,8 @@ Lcam=K2*[R2 T2];
 [points3d] = mytriangulate(Ipoints, IpointsR, Rcam,Lcam );
 plot3(points3d(:,1),points3d(:,2),points3d(:,3),'r.')
 
-CR=-inv(R)*T;
-CL=-inv(R2)*T2;
+CL=-inv(R)*T;
+CR=-inv(R2)*T2;
 plot3(CR(1),CR(2),CR(3),'gs');
 plot3(CL(1),CL(2),CL(3),'cs');
 axis equal
